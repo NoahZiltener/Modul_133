fdd2fa883bcc413f11c15d4704b01fa4445661f0
Noah <noahziltener@yahoo.com>
2020-09-10 09:25:32 +0200

    added task 3

--- Block3/Aufgabe3/script.js
+++ Block3/Aufgabe3/script.js
@@ -1,3 +1,12 @@
 function printFibonacci(){
-    console.log("Test")
+    console.log("Fibonacci Numbers:")
+    var inputnumber = document.getElementById("number").value;
+    var a = 1, b = 0, temp;
+    while(a < inputnumber){
+        console.log(a);
+        temp = a;
+        a = a + b;
+        b = temp;
+        
+    }
 }
